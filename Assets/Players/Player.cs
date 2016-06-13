97972289f6352af6039dfb9b03a6c6f42b26d21f
Stefan Worzalla <stewo1337@gmail.com>
2016-06-14 03:03:35 +0700

    Commit for DevBuild V0.041

--- Assets/Players/Player.cs
+++ Assets/Players/Player.cs
@@ -18,4 +18,8 @@ public class Player : NetworkBehaviour {
 
 		transform.Translate(inputValue);
 	}
+
+	public override void OnStartLocalPlayer() {
+		GetComponentInChildren<Camera>().enabled = true;
+	}
 }
