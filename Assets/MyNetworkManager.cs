2477a62d13dadacd04fd048f9148f268671fe447
Stefan Worzalla <stewo1337@gmail.com>
2016-06-14 01:45:22 +0700

    Commit for Build V0.01

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+	public void MyStartHost() {
+		Debug.Log(Time.timeSinceLevelLoad + " Starting Host");
+		StartHost();
+	}
+
+	public override void OnStartHost() {
+		Debug.Log(Time.timeSinceLevelLoad + " Host started.");
+	}
+
+	public override void OnStartClient(NetworkClient myClient) {
+		Debug.Log(Time.timeSinceLevelLoad + " Client start requested.");
+	}
+
+	public override void OnClientConnect(NetworkConnection conn) {
+		Debug.Log(Time.timeSinceLevelLoad + " Client is connected to IP: " + conn.address);
+	}
+}
